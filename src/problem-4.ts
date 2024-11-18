d3c86012d1d4fd599d95fc2048fe4cc89fbb8ecc
ashiqur rahman biplop <biplops22@gmail.com>
2024-11-18 17:13:02 +0600

    problem - 4 is complete

---
+++ src/problem-4.ts
@@ -0,0 +1,31 @@
+type Circle = {
+    shape: "circle";
+    radius: number;
+}
+type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number
+}
+
+type UniqueShape = Circle | Rectangle;
+
+const calculateShapeArea = (shape: UniqueShape): number => {
+    if (shape.shape === "circle") {
+        return parseFloat((Math.PI * shape.radius * shape.radius).toFixed(2))
+    } else if (shape.shape === "rectangle") {
+        return shape.height * shape.width
+    }
+    return 0
+}
+
+
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+// console.log(circleArea);
+
+const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+});
+console.log(rectangleArea); 
